efaf8024aa7cf2937c94210710e406836cd8257f
leto-007 <liga07@bk.ru>
2025-07-27 00:47:49 +0300

    переменые задача 1

---
+++ src/main/java/HogwartsTeachers.java
@@ -0,0 +1,12 @@
+public class HogwartsTeachers {
+    public static void main(String[] args) {
+        String dambldore = "damboldore\n";
+        String mcgonagal = "mcgonagal\n";
+        String lupin = "lupin\n";
+        String snape = "smape\n";
+        String hagrid = "hagrid\n";
+        String teachers = "School teachers:\n";
+        System.out.println(teachers + dambldore + mcgonagal + lupin + snape + hagrid);
+    }
+    
+}
