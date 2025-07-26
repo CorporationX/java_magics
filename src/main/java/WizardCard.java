b0f0aed53bd359fdb934eccb19f7a4600b8578e8
leto-007 <liga07@bk.ru>
2025-07-27 01:15:32 +0300

    решил вторую задачу

---
+++ src/main/java/WizardCard.java
@@ -0,0 +1,9 @@
+public class WizardCard {
+    public static void main(String[] args) {
+       String name ="gary poter";
+       int age = 11;
+       double height = 1.45;
+       boolean wand = true;
+       System.out.println("Имя ученика:\n" + name + "\n" + "Возраст:\n" + age + "\n" + "Рост:\n" + height + "\n" + "Есть палочка:\n" + wand);
+    }
+}
