0a771088048ce08cfc448ee5dcf2e6ca8bad32d1
Lowdude1 <sanitaronlain@gmail.com>
2025-07-27 21:28:38 +0300

    решение 5 задачи

--- src/main/java/QuidditchCompetition.java
+++ src/main/java/QuidditchCompetition.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class QuidditchCompetition {
+    public static void main(String[] args){
+        String name = "Гарри Поттер";
+        int age = 13;
+        boolean hasBroom = true;
+        boolean passedMedicalCheck = true;
+        int trainingSessionsAttended = 4;
+        boolean canPlay = false;
+        if(age > 11 && hasBroom && passedMedicalCheck && trainingSessionsAttended > 3)
+            canPlay = true;
+        System.out.println(canPlay);
+    }
 }
