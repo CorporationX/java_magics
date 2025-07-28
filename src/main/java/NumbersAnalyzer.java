218c7519cf20a13840046ee5079b34bcb338a073
Lowdude1 <sanitaronlain@gmail.com>
2025-07-28 20:53:01 +0300

    решение 15 задачи

---
+++ src/main/java/NumbersAnalyzer.java
@@ -0,0 +1,16 @@
+public class NumbersAnalyzer {
+    public static void main(String[] args){
+        int[] num = {1, 14, 7, 31, 53};
+        int min = 100;
+        int max = 0;
+        for (int i = 0; i<num.length; i++){
+            if(num[i]<min){
+                min = num[i];
+            }
+            if(num[i]>max){
+                max = num[i];
+            }
+        }
+        System.out.println("Минимальное число: " + min + "\n" + "Максимальное число: " + max);
+    }
+}
