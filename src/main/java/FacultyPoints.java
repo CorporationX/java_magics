ceffd932f730e0ec74e6c5778cb471146c155f97
pinchalex <pinchalex484@gmail.com>
2025-07-28 20:57:37 +0300

    Решил третью задачу.

---
+++ src/main/java/FacultyPoints.java
@@ -0,0 +1,12 @@
+public class FacultyPoints {
+    public static void main(String[] args) {
+        int gryffindorPoints = 150;
+        gryffindorPoints += 20;
+        gryffindorPoints += 10;
+        gryffindorPoints -= 30;
+        gryffindorPoints += 50;
+        gryffindorPoints -= 1;
+
+        System.out.println("Итоговые баллы факультета Гриффиндор: " + gryffindorPoints);
+    }
+}
