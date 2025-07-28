ad633536e228b11720447c5526c8843487f5dec7
pinchalex <pinchalex484@gmail.com>
2025-07-28 17:22:30 +0300

    Решил первую задачу.

---
+++ src/main/java/HogwartsTeachers.java
@@ -0,0 +1,19 @@
+public class HogwartsTeachers {
+    public static void main(String[] args) {
+        String dumbledore = "Albus Dumbledore";
+        String mcgonagall = "Minerva McGonagall";
+        String snape = "Severus Snape";
+        String lupen = "Remus Lupin";
+        String hagrid = "Rubeus Hagrid";
+
+        String teachers = "School teachers:";
+
+        System.out.println(teachers);
+        System.out.println(dumbledore);
+        System.out.println(mcgonagall);
+        System.out.println(snape);
+        System.out.println(lupen);
+        System.out.println(hagrid);
+
+    }
+}
