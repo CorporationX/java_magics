23c5f486b283bfcf339cece016d7775a280fdadf
pinchalex <pinchalex484@gmail.com>
2025-07-30 15:24:35 +0300

    Решение девятой задачи (for, сумма четных чисел)

---
+++ src/main/java/EvenNumbers.java
@@ -0,0 +1,13 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 222; i <= 444; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+                System.out.println("ТЕКУЩЕЕ четное число: " + i);
+                System.out.println("ТЕКУЩАЯ Сумма всех четных чисел: " + sum + "\n");
+            }
+        }
+        System.out.println("Сумма всех четных чисел: " + sum);
+    }
+}
