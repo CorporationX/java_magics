34f1e6275d4aa27e3be76e190e21f5342bf31bed
AndreySN7 <andrey__n@mail.ru>
2025-07-31 18:05:30 +0300

    поправил решение 8 задачи

--- src/main/java/MagicNumbers.java
+++ src/main/java/MagicNumbers.java
@@ -10,19 +10,16 @@ public class MagicNumbers {
         if (number1 % 2 == 0 && number2 % 2 == 0) {
             System.out.println("Оба числа четные. Каждое число делим на 2 и находим их сумму");
             sum = number1 / 2 + number2 / 2;
-            System.out.println("Сумма = " + sum);
         } else if (number1 % 2 == 0) {
             System.out.println("Только первое число четное. Делим его на 2 и складываем со вторым числом");
             sum = number1 / 2 + number2;
-            System.out.println("Сумма = " + sum);
         } else if (number2 % 2 == 0) {
             System.out.println("Только второе число четное. Делим его на 2 и складываем с первым числом");
             sum = number1 + number2 / 2;
-            System.out.println("Сумма = " + sum);
         } else {
             System.out.println("Оба числа нечетные. Находим их сумму");
             sum = number1 + number2;
-            System.out.println("Сумма = " + sum);
         }
+        System.out.println("Сумма = " + sum);
     }
 }
