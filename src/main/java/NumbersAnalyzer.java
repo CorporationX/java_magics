abda4af9090773604c5f9e1bee355a94f804cf6d
AndreySN7 <andrey__n@mail.ru>
2025-07-31 18:45:15 +0300

    поправил решение 15 задачи

--- src/main/java/NumbersAnalyzer.java
+++ src/main/java/NumbersAnalyzer.java
@@ -1,24 +1,22 @@
+import java.util.Arrays;
 import java.util.Random;
+
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
 
 public class NumbersAnalyzer {
     public static void main(String[] args) {
         // Создаем массив из 5 целых чисел и заполняем его рандомными значениями от -50 до 50
         int[] arr = new int[5];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = new Random().nextInt(-50, 51);
-        }
+        Random random = new Random();
+        int min = MAX_VALUE;
+        int max = MIN_VALUE;
 
-        // выводим числа на экран
-        for (int array : arr) {
-            System.out.print(array + " ");
-        }
-
-        // находим минимально и максимальное значения массива
-        int min = arr[0];
-        int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <= min) min = arr[i];
-            if (arr[i] > max) max = arr[i];
+            arr[i] = random.nextInt(-50, 51);
+            System.out.print(arr[i] + " ");    // выводим числа на экран
+            if (arr[i] <= min) min = arr[i];   // находим минимальное
+            if (arr[i] > max) max = arr[i];    // и максимальное значения массива
         }
 
         // Выводим min и max значения на экран
