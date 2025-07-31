4739f1cec807d6d9f9b5c3a130d0a23f35da6d2f
AndreySN7 <andrey__n@mail.ru>
2025-07-31 23:41:04 +0300

    Решение 16 задачи

---
+++ src/main/java/Wand.java
@@ -0,0 +1,12 @@
+public class Wand {
+    String woodType;
+    String core;
+    int length;
+
+    // Конструктор
+    public Wand(String woodType, String core, int length) {
+        this.woodType = woodType;
+        this.core = core;
+        this.length = length;
+    }
+}
