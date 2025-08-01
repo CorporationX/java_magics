559372ace5e122f7809bc1880b0824cbb9505b73
pinchalex <pinchalex484@gmail.com>
2025-08-01 17:49:31 +0300

    Решил 16-ю задачу.

---
+++ src/main/java/WandMain.java
@@ -0,0 +1,10 @@
+public class WandMain {
+    public static void main(String[] args) {
+        Wand wand1 = new Wand("Тис", "Перо феникса", 34);
+        System.out.println("Палочка 1: " + wand1.woodType + ", ядро: " + wand1.core + ", длина: " + wand1.length + " см");
+
+        Wand wand2 = new Wand("Ясень", "Волос единорога", 30);
+        System.out.println("Палочка 2: " + wand2.woodType + ", ядро: " + wand2.core + ", длина: " + wand2.length + " см");
+
+    }
+}
