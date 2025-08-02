9c50f20bb2dcb9007b4e17a645cf6338c00f8a52
AndreySN7 <andrey__n@mail.ru>
2025-08-02 21:08:26 +0300

    ООП: База. Методы. Решение 21* задачи

---
+++ src/main/java/Faculty.java
@@ -0,0 +1,47 @@
+public class Faculty {
+    String name;   // название факультета
+    int points;    // текущее количество очков
+    int penalties; // количество штрафов
+
+    public Faculty(String name) {
+        this.name = name;
+        this.points = 0;
+        this.penalties = 0;
+    }
+
+    public Faculty(String name, int points) {
+        this.name = name;
+        this.points = points;
+        this.penalties = 0;
+    }
+
+    // прибавляет очки
+    void addPoints(int value) {
+        this.points += value;
+    }
+
+    // увеличивает счётчик штрафов на 1 и отнимает 5 очков.
+    void addPenalty() {
+        this.penalties++;
+        this.points -= 5;
+    }
+
+    // печатает название факультета, очки и количество штрафов.
+    void printInfo() {
+        System.out.println("Факультет: " + name
+                + "\nОчки: " + points
+                + "\nШтрафы: " + penalties + "\n");
+    }
+
+    // возвращает true, если текущий факультет лидирует по очкам над переданным факультетом.
+    boolean isLeader(Faculty other) {
+        return this.points > other.points;
+    }
+
+    static void leader(Faculty faculty1, Faculty faculty2) {
+        if (faculty1.isLeader(faculty2)) {
+            System.out.println("Лидирует: " + faculty1.name);
+        }
+        else System.out.println("Лидирует: " + faculty2.name);
+    }
+}
---
+++ src/main/java/FacultyMain.java
@@ -0,0 +1,34 @@
+public class FacultyMain {
+    public static void main(String[] args) {
+//        Создайте несколько факультетов (например, Гриффиндор и Слизерин).
+//        Проведите несколько событий: например, Гриффиндор получает 20 очков,
+//         Слизерин получает 50 очков, затем Гриффиндор получает штраф и т.д.
+//        Выведите информацию о факультетах
+//        Определите, какой из факультетов лидирует.
+        Faculty griffindor = new Faculty("Гриффиндор");
+        Faculty slizerin = new Faculty("Слизерин", 5);
+
+        griffindor.addPoints(25);
+        slizerin.addPoints(50);
+        griffindor.addPenalty();
+        slizerin.addPenalty();
+        griffindor.addPoints(50);
+        griffindor.addPenalty();
+        slizerin.addPoints(10);
+
+        griffindor.printInfo();
+        slizerin.printInfo();
+
+        Faculty.leader(griffindor, slizerin);
+
+        // тестируем дальше
+        System.out.println();
+
+        slizerin.addPoints(10);
+        griffindor.addPenalty();
+
+        griffindor.printInfo();
+        slizerin.printInfo();
+        Faculty.leader(slizerin, griffindor);
+    }
+}
