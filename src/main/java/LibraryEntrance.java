e05146cffd5a08bd41d70b66eb104789cc9aadff
AndreySN7 <andrey__n@mail.ru>
2025-08-03 20:12:03 +0300

    ООП: База. Модификаторы доступа. Альтернативное решение 24 задачи

---
+++ src/main/java/LibraryEntrance.java
@@ -0,0 +1,18 @@
+public class LibraryEntrance {
+    public static void main(String[] args) {
+        LibraryAccessChecker libraryAccessChecker = new LibraryAccessChecker();
+
+        System.out.println("Проверка доступа в Запретную Секцию...\n");
+
+        libraryAccessChecker.checkLibraryAccess("Гарри Поттер", 3);
+        libraryAccessChecker.printAccess();
+        libraryAccessChecker.checkLibraryAccess("Гермиона Грейнджер", 3);
+        libraryAccessChecker.printAccess();
+
+        boolean checkSedrik = libraryAccessChecker.checkLibraryAccess("Седрик Диггори", 4);
+        System.out.println(checkSedrik); // Проверка доступа на печать
+        libraryAccessChecker.printAccess();
+    }
+
+
+}
