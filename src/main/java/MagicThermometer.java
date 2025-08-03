c50ee946dace589f5e28b4d3a7d43a4c48c0985f
AndreySN7 <andrey__n@mail.ru>
2025-08-03 16:00:57 +0300

    ООП: База. Модификаторы доступа, инкапсуляция, геттеры и сеттеры. Решение 22 задачи/ Внесены некоторые правки

--- src/main/java/MagicThermometer.java
+++ src/main/java/MagicThermometer.java
@@ -1,22 +1,8 @@
 public class MagicThermometer {
     private int temperature = 10;
 
-//    Тестирование
-
 //    public static void main(String[] args) {
-//        MagicThermometer thermometer = new MagicThermometer();
-//        System.out.println(thermometer.getTemperature());  // ➡ 10°C (начальная температура)
-//
-//        thermometer.increase(50);
-//        System.out.println(thermometer.getTemperature());  // ➡ 60°C
-//
-//        thermometer.increase(50);
-//        thermometer.increase(-50); // Отрицательное значение
-//        System.out.println(thermometer.getTemperature());  // ➡ 100°C (и предупреждение)
-//
-//        thermometer.decrease(95);
-//        thermometer.decrease(-95); // Отрицательное значение
-//        System.out.println(thermometer.getTemperature());  // ➡ 10°C (и предупреждение)
+
 //    }
 
     public int getTemperature() {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,26 @@ public class Main {
 //        System.out.println("Палочка 2: " + wand2.woodType + ", ядро: " + wand2.core + ", длина: " + wand2.length + " см");
 
         // Задача 17
-        Wand harryWand = new Wand("Тис", "Перо феникса", 34);
-        HogwartsStudent harry = new HogwartsStudent("Гарри Поттер", 11, harryWand);
-        System.out.println("Ученик: " + harry.name + ", возраст: " + harry.age + ", палочка: " + harry.wand.woodType);
+//        Wand harryWand = new Wand("Тис", "Перо феникса", 34);
+//        HogwartsStudent harry = new HogwartsStudent("Гарри Поттер", 11, harryWand);
+//        System.out.println("Ученик: " + harry.name + ", возраст: " + harry.age + ", палочка: " + harry.wand.woodType);
+//
+//        HogwartsStudent tom = new HogwartsStudent("Том Реддл", 16);
+//        System.out.println("Ученик: " + tom.name + ", возраст: " + tom.age + ", палочка: нет");
 
-        HogwartsStudent tom = new HogwartsStudent("Том Реддл", 16);
-        System.out.println("Ученик: " + tom.name + ", возраст: " + tom.age + ", палочка: нет");
+        // Задача 22
+//        MagicThermometer thermometer = new MagicThermometer();
+//        System.out.println(thermometer.getTemperature());  // ➡ 10°C (начальная температура)
+//
+//        thermometer.increase(50);
+//        System.out.println(thermometer.getTemperature());  // ➡ 60°C
+//
+//        thermometer.increase(50);
+//        thermometer.increase(-50); // Отрицательное значение
+//        System.out.println(thermometer.getTemperature());  // ➡ 100°C (и предупреждение)
+//
+//        thermometer.decrease(95);
+//        thermometer.decrease(-95); // Отрицательное значение
+//        System.out.println(thermometer.getTemperature());  // ➡ 10°C (и предупреждение)
     }
 }
