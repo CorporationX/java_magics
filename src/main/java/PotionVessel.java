279c113e98769b5b97bba66ca1656a7bed2d5d02
Роберт <i@rsirazetdinow.ru>
2025-08-03 19:01:47 +0300

    Задание 18 Хочу его сделать лучше, кажется его можно сильно доработать, ушло много часов

---
+++ src/main/java/PotionVessel.java
@@ -0,0 +1,19 @@
+class PotionVessel {
+    String potionName;
+    double volume;
+    boolean inMagical;
+    HogwartsStudent creator;
+
+    PotionVessel(String potionName, double volume, boolean inMagical, HogwartsStudent creator) {
+        this.potionName = potionName;
+        this.volume = volume;
+        this.inMagical = inMagical;
+        this.creator = creator;
+    }
+    PotionVessel(double volume, boolean inMagical, HogwartsStudent creator) {
+        this.potionName = "Неизвестно";
+        this.volume = volume;
+        this.inMagical = false;
+        this.creator = creator;
+    }
+}
