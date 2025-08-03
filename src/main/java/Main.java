506794fcb277f3be40c238118920433ae2f18da7
AndreySN7 <andrey__n@mail.ru>
2025-08-03 18:02:04 +0300

    ООП: База. Модификаторы доступа, инкапсуляция, геттеры и сеттеры. Решение 23 задачи

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,5 +29,34 @@ public class Main {
 //        thermometer.decrease(95);
 //        thermometer.decrease(-95); // Отрицательное значение
 //        System.out.println(thermometer.getTemperature());  // ➡ 10°C (и предупреждение)
+
+        // Задача 23
+        MagicHourglass hourglass = new MagicHourglass();
+
+        System.out.println("Осталось: " + hourglass.getTime() + " секунд"); // ➡ Осталось: 60 секунд
+
+        hourglass.tick();
+        hourglass.tick();
+        System.out.println("Осталось: " + hourglass.getTime() + " секунд"); // ➡ Осталось: 58 секунд
+
+        hourglass.flip(45); // ➡ 🔄 Вы перевернули песочные часы! Времени теперь: 45 секунд.
+        System.out.println("Осталось: " + hourglass.getTime() + " секунд"); // ➡ Осталось: 45 секунд
+
+        hourglass.stop(); // ➡ ⏳ Часы остановлены.
+        hourglass.tick(); // ➡ ⏳ Часы не идут! Переверните их.
+
+        hourglass.start(); // ➡ ▶ Часы снова идут!
+
+        hourglass.flip(5); // ➡ 🔄 Вы перевернули песочные часы! Времени теперь: 60 секунд.
+        System.out.println("Осталось: " + hourglass.getTime() + " секунд"); // ➡ Осталось: 60 секунд
+
+        hourglass.flip(10);
+//        hourglass.start(); // ➡ ▶ Часы снова идут!
+        for (int i = 1; i <= 9; i++) {
+            hourglass.tick();
+        }
+        hourglass.tick();
+//        hourglass.flip(75);
+        System.out.println("Осталось: " + hourglass.getTime() + " секунд"); // ➡ Осталось: 1 секунда
     }
 }
