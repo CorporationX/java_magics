63c69067a0c218621ed6be93d09ace6ca0afd87f
reiqe <reiqen@proton.me>
2025-08-04 21:26:51 +0500

    Add BigDecimal solution

--- src/main/java/BigDecimalSolution.java
+++ src/main/java/BigDecimalSolution.java
@@ -1,22 +1,24 @@
+import java.math.BigDecimal;
+
 public class BigDecimalSolution {
     public static void main(String[] args) {
         // Зелье варится, делаем 5 замеров температуры, считаем среднюю температуру, делаем проверку на
         // перекипело или нет
 
         // TODO домашнее задание - перевести все double в BigDecimal
-        double[] temperatures = {0.1f, 0.2f};
+        BigDecimal[] temperatures = {new BigDecimal("0.1"), new BigDecimal("0.2")};
 
-        double sum = 0;
-        for (double temperature : temperatures) {
-            sum = sum + temperature;
+        BigDecimal sum = BigDecimal.ZERO;
+        for (BigDecimal temperature : temperatures) {
+            sum = sum.add(temperature);
         }
         System.out.println("Сумма: " + sum);
 
-        double average = sum / temperatures.length;
+        BigDecimal average = sum.divide(new BigDecimal(temperatures.length));
 
         System.out.println("Среднее число: " + average); // Not a number
 
-        if (average > 80) {
+        if (average.doubleValue() > 80) {
             System.out.println("Перекипело!");
         } else {
             System.out.println("Все ок");
