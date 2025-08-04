4429dbd265f7cf29f4c473dfc2eec5787890b445
Роберт <i@rsirazetdinow.ru>
2025-08-04 13:39:44 +0300

    Исправил 15 задание

--- src/main/java/NumbersAnalyzer.java
+++ src/main/java/NumbersAnalyzer.java
@@ -1,11 +1,11 @@
 public class NumbersAnalyzer {
     public static void main(String[] args) {
-        int[] num  = {13,2,34,4,35};
+        int[] nums  = {13,2,34,4,35};
 
-        int min = num[0];
-        int max = num[0];
+        int min = nums[0];
+        int max = nums[0];
         // всё что ниже мне предложил исправить IJ
-        for (int number : num) { //эту строку я понял только от части что значит ": num"?
+        for (int number : nums) { //эту строку я понял только от части что значит ": num"?
             if (number < min) min = number;
 
             if (number > max) max = number;
