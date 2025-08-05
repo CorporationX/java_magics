a466359b0108d4231c42c063f3abab686a9ee4f1
Роберт <i@rsirazetdinow.ru>
2025-08-05 18:13:12 +0300

    20 задание

---
+++ src/main/java/PotionLab.java
@@ -0,0 +1,19 @@
+public class PotionLab {
+    public static void main(String[] args) {
+        int students = 15;
+        String potion = "Оборотное зелье";
+
+        System.out.println("Расчет для " + students + " порций " + potion + ":");
+
+        PotionCalculator calculator = new PotionCalculator();
+
+        int slugsNeeded = calculator.calculateNeededSlugs(students);
+        int fluxweedNeeded = calculator.calculateNeededFluxweed(students);
+
+        System.out.println("Необходимо сушеных пиявок: " + slugsNeeded);
+        System.out.println("Необходимо пучков водорослей: " + fluxweedNeeded);
+
+        String label = calculator.getPotionLabel(potion, students, slugsNeeded, fluxweedNeeded);
+        System.out.println(label);
+    }
+}
