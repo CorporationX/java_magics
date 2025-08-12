f15b93f7da3d6b3af9083ed128a9b2afcfb06750
ulyanmyanyan <chemical.fawn@gmail.com>
2025-08-12 15:01:18 +0500

    Не доделала задачу 5

---
+++ src/main/java/QuidditchCompetition.java
@@ -0,0 +1,17 @@
+public class QuidditchCompetition {
+    public static void main(String[] args) {
+        // Данные
+        String name = "Гарри";
+        int age = 13;
+        boolean hasBroom = true;
+        boolean passedMedicalCheck = true;
+        int trainingSessionsAttended = 4;
+
+        // Условия к допуску
+        boolean canPlay = age > 11 && hasBroom && passedMedicalCheck && trainingSessionsAttended > 3;
+        if
+
+        // Вывод
+        System.out.println(canPlay);
+    }
+}
