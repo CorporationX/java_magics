019f5656aaf09e4732ea5fed83a8115dc5c4d179
ulyanmyanyan <chemical.fawn@gmail.com>
2025-08-13 22:44:45 +0500

    Решила задачу 10.

---
+++ src/main/java/Quidditch.java
@@ -0,0 +1,15 @@
+public class Quidditch {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            // Определяем бросок
+            if (i == 1) {
+            System.out.println("Запущен мяч под названием: Quaffle");
+            } else if (i == 2) {
+            System.out.println("Запущен мяч под названием: Bludger");
+            } else if (i ==3) {
+                System.out.println("Запущен мяч под названием: Golden Snitch");
+            }
+
+        }
+    }
+}
