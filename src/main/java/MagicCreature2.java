2433bdfb6fd9bf0edfcefd8b8878af1d837a28e8
Lowdude1 <sanitaronlain@gmail.com>
2025-08-13 13:38:42 +0300

    Решение 31 задачи

---
+++ src/main/java/MagicCreature2.java
@@ -0,0 +1,45 @@
+public class MagicCreature2 {
+    String name;
+    int power;
+    static int count = 0;
+    static int maxPower = 0;
+
+    public MagicCreature2(String name, int power) {
+        this.name = name;
+        this.power = power;
+        count++;
+
+        if (power > maxPower) {
+            maxPower = power;
+        }
+    }
+
+    // Геттеры для привыкания просто)
+    public String getName() {
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static int getMaxPower() {
+        return maxPower;
+    }
+
+    // Можно его сделать статическим и вызывать через класс
+    static void printStatistics() {
+        System.out.println("Создано существ: " + getCount());
+        System.out.println("Максимальная сила: " + getMaxPower());
+    }
+
+    // Но хорошая логика будет если написать метод нестатичный для отдельных существ, например:
+    void printInfoCreature() {
+        System.out.println("\nИмя существа: " + getName());
+        System.out.println("Сила существа: " + getPower());
+    }
+}
