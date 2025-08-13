514f402a50ec8b7b9c14e873df0ee8a5045b3c61
pinchalex <pinchalex484@gmail.com>
2025-08-13 18:40:15 +0300

    Решение 28 задачи на проверку.

---
+++ src/main/java/task28/SpellChangesEverything.java
@@ -0,0 +1,21 @@
+package task28;
+
+public class SpellChangesEverything {
+    public static void main(String[] args) {
+        castSpell();
+    }
+
+    public static void castSpell() {
+        String spell = "ARDENTIS VERUM LUMINOS ET FULGUR SYLVESTRA ELIXIA";
+
+        String lowerSpell = spell.toLowerCase();
+        String halfLowerSpell = lowerSpell.substring(lowerSpell.length() / 2);
+
+        StringBuilder builderSpell = new StringBuilder(halfLowerSpell);
+        builderSpell.reverse();
+        builderSpell.replace(0,1, "!");
+        builderSpell.replace(1,2, " ");
+
+        System.out.println("Итоговая строка: " + builderSpell.toString());
+    }
+}
