15a995f0df2fbc58ff75c507bbee4d19a42cb650
ulyanmyanyan <chemical.fawn@gmail.com>
2025-08-17 19:50:23 +0500

    Решила задачу 16.

--- src/main/java/Wand.java
+++ src/main/java/Wand.java
@@ -1,3 +1,4 @@
+
 Class Wand {
 String woodType; //
 String core;
@@ -5,17 +6,17 @@ int length;
 
 //Конструктор
 Wand(String woodType, String core, int length) {
-    this.woodType = woodType;
-    this.core = core;
-    this.length = length;
+    this.woodType = woodType ;
+    this.core = core ;
+    this.length = length ;
 }
         }
 
-public class WandMine  {
+public class Wand  {
     public static void main(String[] args) {
         Wand wand1 = new Wand("Бузина", "Волос фестрала", "38");
-        System.out.println("Палочка 1"+wand1.woodType +", ядро"+wand1.core +", длина"+wand1.length +"см.");
+        System.out.println("Палочка 1" + wand1.woodType + ", ядро" + wand1.core + ", длина" + wand1.length + "см.");
         Wand wand2 = new Wand("Вяз", "Сердечная жила дракона", "45");
-        System.out.println("Палочка 2"+wand2.woodType +", ядро"+wand2.core +", длина"+wand2.length +"см.");
+        System.out.println("Палочка 2" + wand2.woodType +", ядро" + wand2.core + ", длина"+wand2.length + "см.");
     }
 }
