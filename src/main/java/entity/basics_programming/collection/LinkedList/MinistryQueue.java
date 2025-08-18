2cc07a8978a36de810ce87df6bbbd5e2496e0d2d
AndreySN7 <andrey__n@mail.ru>
2025-08-18 08:40:16 +0300

    Collections. LinkedList. Решил 53 задачу

--- src/main/java/entity/basics_programming/collection/LinkedList/MinistryQueue.java
+++ src/main/java/entity/basics_programming/collection/LinkedList/MinistryQueue.java
@@ -1,10 +1,11 @@
 package entity.basics_programming.collection.LinkedList;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class MinistryQueue {
     public static void main(String[] args) {
-        LinkedList<String> queue = new LinkedList<>();
+        List<String> queue = new LinkedList<>();
         queue.add("Гарри");
         queue.add("Рон");
         queue.add("Гермиона");
