f1b36e30a9b817086e259ae7065b0b7a370bb93a
AndreySN7 <andrey__n@mail.ru>
2025-08-18 19:58:29 +0300

    Исключения. Логирование. Поправил 49 задачу

--- src/main/java/entity/basics_programming/exceptions/task49/HogwartsWatcher.java
+++ src/main/java/entity/basics_programming/exceptions/task49/HogwartsWatcher.java
@@ -7,28 +7,22 @@ public class HogwartsWatcher {
 
     public static void scanLocation(String location, String activity) {
         switch (location) {
-            case ("Запретный Лес"): // любая активность должна логироваться как SEVERE.
-                logger.severe("Критическая угроза! " + activity);
-                break;
-            case ("Тайная Комната"): // любая активность должна логироваться как WARNING.
-                logger.warning("Подозрительная активность! " + activity);
-                break;
-            case ("Гостиная Гриффиндора"):
-                // если activity содержит слово "заклинание" логировать как WARNING. В остальных случаях — как INFO.
-                if (activity.contains("заклинание")) {
+            // любая активность должна логироваться как SEVERE.
+            case ("Запретный Лес") -> logger.severe("Критическая угроза! " + activity);
+            // любая активность должна логироваться как WARNING.
+            case ("Тайная Комната") -> logger.warning("Подозрительная активность! " + activity);
+            // если activity содержит слово "заклинание" логировать как WARNING. В остальных случаях — как INFO.
+            case ("Гостиная Гриффиндора") -> {
+                if (activity.contains("заклинание"))
                     logger.warning("Подозрительная активность. " + activity);
-                } else {
+                else
                     logger.info("Обычная активность. " + activity);
-                }
-                break;
-
-            case ("Кабинет Дамблдора"):
-                // логировать только SEVERE ошибки, если activity содержит "Волан-де-Морт".
-                // В остальных случаях логировать ничего не нужно.
-                if (activity.contains("Волан-де-Морт")) {
-                    logger.severe("Критическая угроза! " + activity);
-                }
-                break;
+            }
+            // логировать только SEVERE ошибки, если activity содержит "Волан-де-Морт".
+            // В остальных случаях логировать ничего не нужно.
+            case ("Кабинет Дамблдора") -> {
+                if (activity.contains("Волан-де-Морт")) logger.severe("Критическая угроза! " + activity);
+            }
         }
     }
 }
