a894aa2409109795ae1289338e62d43ca9e754d4
Lowdude1 <sanitaronlain@gmail.com>
2025-08-18 18:58:49 +0300

    Исправление 43 задачи

--- src/main/java/Exceptions/SpellTrainer.java
+++ src/main/java/Exceptions/SpellTrainer.java
@@ -1,23 +1,15 @@
 package Exceptions;
 
-public class SpellTrainer
-{
-    public static void main(String[] args)
-    {
-        String[] spells = { "Expelliarmus", "Avada Kedavra", "Lumos", "Perceptio Recupero" };
+public class SpellTrainer {
+    public static void main(String[] args) {
+        String[] spells = {"Expelliarmus", "Avada Kedavra", "Lumos", "Perceptio Recupero"};
 
-        for (String spell : spells)
-        {
-            try
-            {
+        for (String spell : spells) {
+            try {
                 castSpell(spell);
-            }
-            catch (IllegalArgumentException e)
-            {
+            } catch (IllegalArgumentException e) {
                 System.out.println("Ошибка обезоруживания: " + e.getMessage());
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Общая магическая ошибка: " + e.getMessage());
             }
 
@@ -25,32 +17,18 @@ public class SpellTrainer
         }
     }
 
-    public static void castSpell(String spellName)
-    {
+    public static void castSpell(String spellName) {
         System.out.println("Начинаем произносить заклинание: " + spellName);
 
-        try
-        {
-            if (spellName.equals("Expelliarmus"))
-            {
-                // ещё не прошли, но я посмотрел про исключения статью, без throw нам не выкинуть исключение
-                throw new IllegalArgumentException("Заклинание обезоруживания дало сбой");
+        try {
+            switch (spellName) {
+                case "Expelliarmus" ->
+                    // ещё не прошли, но я посмотрел про исключения статью, без throw нам не выкинуть исключение
+                        throw new IllegalArgumentException("Заклинание обезоруживания дало сбой");
+                case "Lumos" -> System.out.println("Заклинание успешно выполнено");
+                default -> throw new RuntimeException("Заклинание запрещено!");
             }
-            else if (spellName.equals("Avada Kedavra"))
-            {
-                throw new RuntimeException("Заклинание запрещено!");
-            }
-            else if (spellName.equals("Lumos"))
-            {
-                System.out.println("Заклинание успешно выполнено");
-            }
-            else
-            {
-                throw new RuntimeException("Заклинание запрещено!");
-            }
-        }
-        finally
-        {
+        } finally {
             System.out.println("Закрываем поток магической энергии");
         }
     }
