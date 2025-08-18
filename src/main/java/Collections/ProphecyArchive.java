cc80980b8b2c7c3218e7b008b9aeeb050fb5fa3b
Lowdude1 <sanitaronlain@gmail.com>
2025-08-19 00:51:47 +0300

    Исправление 50 задачи

---
+++ src/main/java/Collections/ProphecyArchive.java
@@ -0,0 +1,26 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProphecyArchive {
+    public static void main(String[] args) {
+        List<String> prophecy = new ArrayList<>(3);
+        prophecy.add("Пророчество 1");
+        prophecy.add("Пророчество 2");
+        prophecy.add("Пророчество 3");
+
+        for (String prophecyArchive : prophecy) {
+            System.out.println(prophecyArchive);
+        }
+
+        System.out.println("Количество пророчеств: " + prophecy.size());
+        System.out.println("Последнее добавленное пророчество: " + prophecy.getLast());
+        prophecy.remove("Пророчество 2");
+
+        for (String prophecyUpdate : prophecy) {
+            System.out.println(prophecyUpdate);
+        }
+    }
+
+}
