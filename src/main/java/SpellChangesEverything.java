4ccee78efc507f8085158bf786b0eb36424e84a8
Роберт <i@rsirazetdinow.ru>
2025-08-20 14:03:58 +0300

    28 задание изменённое

--- src/main/java/SpellChangesEverything.java
+++ src/main/java/SpellChangesEverything.java
@@ -4,12 +4,10 @@ public class SpellChangesEverything {
     public static void main(String[] args) {
         String spell = "ARDENTIS VERUM LUMINOS ET FULGUR SYLVESTRA ELIXIA";
 
-        String spellLowerCase = spell.toLowerCase(Locale.ROOT);
+        String spellLowerCase = spell.toLowerCase();
         String halfSpell = spellLowerCase.substring(spellLowerCase.length() / 2);
         StringBuilder builder = new StringBuilder(halfSpell);
-        builder.reverse();
-        builder.setCharAt(0, '!');
-        builder.setCharAt(1,' ');
+        builder.replace(0,2, "! ");
 
         System.out.println(builder.toString());
     }
