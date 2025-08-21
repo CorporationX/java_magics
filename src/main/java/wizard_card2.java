dc048c72b97fbb9c50c9f1e934060155eefc5163
Максим <crylov44@yandex.ru>
2025-08-21 19:43:13 +0300

    task 8*

---
+++ src/main/java/wizard_card2.java
@@ -0,0 +1,14 @@
+public class wizard_card2 {
+    public static void main(String[] args) {
+        //Переменные и типы данных
+        //2. Учебная карточка волшебника*
+        String nameGarry = "Гарри Поттер";
+        int ageGarry = 11;
+        double heightGarry = 1.45;
+        boolean magicStick = true;
+        System.out.println("Имя ученика: " + nameGarry);
+        System.out.println("Возраст: " + ageGarry);
+        System.out.println("Рост: " + heightGarry);
+        System.out.println("Есть палочка:" + magicStick);
+    }
+}
