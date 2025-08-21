a5d1a972f1ba51d95ff12a4087090816a1b9a851
ulyanmyanyan <chemical.fawn@gmail.com>
2025-08-21 21:08:02 +0500

    Решила задачу 19.

--- src/main/java/BasicSpellsPractice.java
+++ src/main/java/BasicSpellsPractice.java
@@ -11,22 +11,30 @@ public class BasicSpellsPractice {
 
     // 3. Метод, который принимает название и возвращет строку.
     static String getHouseChant(String houseName) {
-        if (houseName.equals("Гриффиндор"))
+        if (houseName.equals("Гриффиндор")) {
             return "Смелость, отвага, рыцарство!";
-    }else if (houseName.equals("Слизерин")) {
-        return "Хитрость, решительность, амбициозность!";
-    }else if (houseName.equals("Когтевран")) {
-        return "Ум, мудрость, остроумие!";
-    }else (houseName.equals("Пуффендуй")) {
-        return "Трудолюбие, верность, честность!"
+        } else if (houseName.equals("Слизерин")) {
+            return "Хитрость, решительность, амбициозность!";
+        } else if (houseName.equals("Когтевран")) {
+            return "Ум, мудрость, остроумие!";
+        } else if (houseName.equals("Пуффендуй")) {
+            return "Трудолюбие, верность, честность!";
+        } else {
+            return "Данного факультета не существует";
+        }
     }
 
-    // 4. Метод, выводящий main, который выводит все три метода.
-    public static void main(String[] args) {
-        castLumos()
-        int lenght = getWandLength();
-        System.out.println("Длина палочки: " + lenght +" дюймов.");
-        String clant = getHouseChant("Гриффиндор");
-        System.out.println("Девиз факултета:" + clant );
+        // 4. Метод, выводящий main, который выводит все три метода.
+        public static void main (String[]args){
+            // Вызываем метод без возвращаемого значения
+            castLumos();
+
+            // Вызываем метод, возвращающий int, и сохраняем результат
+            int length = getWandLength();
+            System.out.println("Длина палочки: " + length + " дюймов.");
+
+            // Вызываем метод, возвращающий String, и сохраняем результат
+            String chant = getHouseChant("Гриффиндор");
+            System.out.println("Девиз факультета: " + chant);
+        }
     }
-     }
