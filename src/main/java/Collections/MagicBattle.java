f467964181e07549b51f9af62d026f9233d34b22
Lowdude1 <sanitaronlain@gmail.com>
2025-08-23 01:12:37 +0300

    Решение 56 задачи

---
+++ src/main/java/Collections/MagicBattle.java
@@ -0,0 +1,22 @@
+package Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MagicBattle {
+    public static void main(String[] args) {
+        Map<String, String> wizards = new HashMap<>();
+        wizards.put("Harry", "Экспеллиармус");
+        wizards.put("Ron", "Тоталус");
+        if (wizards.containsValue("Neville")) {
+            System.out.println("Заклинание Невилла: " + wizards.get("Neville"));
+        } else {
+            wizards.put("Neville", "Ступефи");
+        }
+
+        System.out.println("Маги и их заклинания: ");
+        for (String wizard : wizards.keySet()) {
+            System.out.println(wizard + ": " + wizards.get(wizard));
+        }
+    }
+}
