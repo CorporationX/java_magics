1f9e83ad93d92d91cd4ab625a751b1ec2e163c9d
Роберт <i@rsirazetdinow.ru>
2025-08-27 00:06:16 +0300

    38 задание на проверку

---
+++ src/main/java/Hippogriff.java
@@ -0,0 +1,11 @@
+class Hippogriff extends MagicalCreature {
+    @Override
+    public void interact() {
+        System.out.println("Гиппогриф кланяется в знак уважения.");
+     }
+
+    @Override
+    public void interact(Muggle muggle) {
+        System.out.println("Гиппогриф смотрит с подозрением.");
+    }
+}
---
+++ src/main/java/MagicalCreature.java
@@ -0,0 +1,9 @@
+class MagicalCreature {
+    public void interact() {
+        System.out.println("Существо ведёт себя нейтрально.");
+    }
+
+    public void interact(Muggle muggle) {
+        System.out.println("Существо странно смотрит на магла.");
+    }
+}
