093b2c909fc40bdd6684da1bb73a7c3e4f8f0146
Роберт <i@rsirazetdinow.ru>
2025-08-27 19:10:02 +0300

    40 задание на проверку

---
+++ src/main/java/MagicDuelsPolymorphism.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class MagicDuelsPolymorphism {
+    public static void main(String[] args) {
+        Wizard[] duelists = {
+                new Student("Гарри Поттер"),
+                new Professor("Профессор МакГонагалл"),
+                new DarkWizard("Воланд-де-Морт"),
+        };
+
+        String[] spells = {"Expelliarmus", "Lumos", "Avada Kedavra"};
+
+        System.out.println("Магическая дуэль начинается!");
+
+        Random rand = new Random();
+
+         int rounds = 2;
+        for (int r = 0; r < rounds; r++) {
+            for (Wizard w : duelists) {
+                String spell = spells[rand.nextInt(spells.length)];
+                boolean continueDuel = w.castSpell(spell);
+                if (!continueDuel) {
+                    // дуэль немедленно прекращается
+                    return;
+                }
+            }
+        }
+
+        System.out.println("Дуэль окончена.");
+    }
+}
